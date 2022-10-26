652bf5e6c4032c774dd01d31218ef17b40ddd321
mallikarjunkerkere <kmallikarjun1602@gmail.com>
2022-10-26 21:03:54 +0530

    Assignment1Java

---
+++ src/AssignmentQ2.java
@@ -0,0 +1,16 @@
+
+public class AssignmentQ2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=6;
+		for(int i=1;i<n-1;i++) {
+			for(int j=0;j<n-2;j++) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
